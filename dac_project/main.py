6cd295b5bb2908ad6d27f79845d548ba8b02ecba
frbi-zvnt <francesco.bianchi.zvnt@outlook.com>
2021-01-28 19:09:59 +0100

    test

--- dac_project/main.py
+++ dac_project/main.py
@@ -8,14 +8,13 @@ from machine import Pin, DAC, Timer
 #for i in range(len(sine)):
 #    sine[i] = 128 + int(127 * math.sin(2 * math.pi * i / len(sine)))
 
-SAMPLERATE = 32000
 
-
+"""
 class Player:
     def __init__(self):
         self.dac = DAC(Pin(25))
         self.square = [0, 255]
-        self.frq = 1
+        self.frq = 1000
         self.counter = 0
         self.tim = Timer(0)
         self.tim.init(freq=SAMPLERATE, mode=Timer.PERIODIC, callback=self.ticker)
@@ -25,28 +24,36 @@ class Player:
         if self.counter >= SAMPLERATE:
             self.counter = 0
 
-        self.play(self.frq)
+        self.play()
 
-    def play(self, frq=1):
-        step = int(frq / SAMPLERATE * self.counter) % 2
+    def play(self):
+        step = int(self.frq / (SAMPLERATE * 2) * self.counter) % 2
         self.dac.write(step * 255)
-
+"""
 
 import random, time 
 
+size = 64
 dac = DAC(Pin(25))
-seq = [100,200,400,800,1600]
-volume = 5
-val = 0
+sine = bytearray(size)
+
+for i in range(size):
+    sine[i] = 128 + int(127 * math.sin(2 * math.pi * i / size))
+
+idx = 0
+
 
 def play(timer):
-    global val, volume 
-    dac.write(val * volume)
-    val = 1 - val 
+    #global idx  
+    for v in sine:
+        #dac.write(sine[idx])
+        dac.write(v)
+    #idx += 1
+    #if idx >= size:
+    #    idx = 0
 
 t = Timer(0)
 
-for f in range(100):
-    t.init(freq=random.choice(seq), mode=Timer.PERIODIC, callback=play)
-    time.sleep(0.1)
+t.init(freq=2000, mode=Timer.PERIODIC, callback=play) 
+
 
